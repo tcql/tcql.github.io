7761a812efdf1bb731d42236add8d28ea714ae16
tcql <tim.channell@gmail.com>
2025-03-20 17:12:15 -0400

    setup sv

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	interface RouteProps {
+		title: string;
+		url: string;
+	}
+	const routes: RouteProps[] = [
+		{ title: 'about', url: '/' },
+		{ title: 'projects', url: '/projects' }
+	];
+</script>
+
+{#snippet pagelink(title: string, url: string)}
+	<li><a href={url}>{title}</a></li>
+{/snippet}
+
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/>
+				</svg>
+			</div>
+			<ul
+				tabindex="0"
+				class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+			>
+				{#each routes as { title, url }}
+					{@render pagelink(title, url)}
+				{/each}
+			</ul>
+		</div>
+		<a class="btn btn-ghost text-xl">tcql</a>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<ul class="menu menu-horizontal px-1">
+			{#each routes as { title, url }}
+				{@render pagelink(title, url)}
+			{/each}
+		</ul>
+	</div>
+	<div class="navbar-end">
+		<a class="btn">Button</a>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="flex flex-col">
+	<Header />
+	<div class="flex-1">
+		<div class="mx-auto max-w-3xl py-8">
+			{@render children()}
+		</div>
+	</div>
+</div>
